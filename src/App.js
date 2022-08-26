4507b22a847d3578228e3214a02024decabc43de
10kareem10 <karimelihab6@gmail.com>
2022-08-26 11:57:17 +0200

    4. adding sign in page, form and styling it

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
-// const HatsPage = () => (
-//   <div>
-//     <h1>
-//       hats page
-//     </h1>
-//   </div>
-// );
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+
 
 function App() {
   return (
@@ -18,6 +14,7 @@ function App() {
       <Switch>
       <Route  exact path='/' component={HomePage}/>
       <Route  path='/shop' component={ShopPage}/>
+      <Route  path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
